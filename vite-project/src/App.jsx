4eb3ef2f349f432958068da84dc10acafd0d148b
ThMenezes <thita.menezes24@gmail.com>
2023-02-15 18:45:14 -0300

    adicionando hooks funções de calculo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,18 +3,21 @@ import Counter from './components/Counter'
 
 import NewYear from './assets/newyear.jpg';
 
+import useCountdown from './hooks/useCountdown';
+
 import './App.css'
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("Mar 1, 2023 00:00:00");
   return (
     <div className="App" style={{backgroundImage: `url(${NewYear})`}}>
       <div className="container">
-        <Title title="Contagem regressiva para o carnaval"/>
+        <Title title="Contagem regressiva para Festa"/>
         <div className="countdown-container">
-          <Counter title="Dias" number={5}/>
-          <Counter title="Horas" number={5}/>
-          <Counter title="Minutos" number={5}/>
-          <Counter title="Segundos" number={5}/>
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/>
         </div>
       </div>
     </div>
